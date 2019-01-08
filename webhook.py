7e7bf5b006941c5740ac364d2bb641dc2ff0076c
Aaron <aherdzina@ebates.com>
2019-01-08 11:07:57 -0600

    Add files via upload
    init

---
+++ webhook.py
@@ -0,0 +1,50 @@
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+
+    res = make_response(req)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def makeResponse(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("get-city")
+    date = parameters.get("date")
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={}, us&appid=aa79484b526bb99411052e16e4b3617d'.format(city))
+    json_object = r.json()
+    weather = json_object['list']
+    condition = None
+    for i in range(0, len(weather)):
+        if date in weather[i]['dt_txt']:
+            condition = weather[i]['weather'][0]['description']
+
+    speech = "The forecast {} in {} is {}".format(city, date, condition)
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "source": "apiai-weather-webhook"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port {}".format(port))
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+
